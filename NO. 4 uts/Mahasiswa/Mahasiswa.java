0499a4f5d28539b14975186ef75f46a3f33c6d51
AnnisaDwiIrvianda <80968962+AnnisaDwiIrvianda@users.noreply.github.com>
2021-04-22 09:20:42 +0700

    Add files via upload

---
+++ NO. 4 uts/Mahasiswa/Mahasiswa.java
@@ -0,0 +1,25 @@
+// Annisa Dwi Irvianda
+// 11200930000009
+// 2A SI
+
+package Mahasiswa;
+
+
+public class Mahasiswa {
+    
+void membaca(){
+    System.out.println("Membaca Menambah Wawasan");
+}
+void menyontek(){
+    System.out.println("Menyontek Sangat Dilarang");
+}
+void modifikasi(){
+    System.out.println("Modifikasi Jumlah Nilai Yang Ada");
+}
+
+void all (){
+    System.out.println("Membaca Menambah Wawasan");
+    System.out.println("Memyontek Sangat Dilarang");
+    System.out.println("Modifikasi Jumlah Nilai Yang Ada");
+}    
+}
---
+++ NO. 4 uts/Mahasiswa/MahasiswaBeraksi.java
@@ -0,0 +1,19 @@
+// Annisa Dwi irvianda
+// 11200930000009
+// 2A SI
+
+package Mahasiswa;
+
+
+public class MahasiswaBeraksi {
+   
+public static void main(String[] args){
+     Mahasiswa Mahasiswa = new Mahasiswa();
+     
+     Mahasiswa.membaca();
+     Mahasiswa.menyontek();
+     Mahasiswa.modifikasi();
+     
+}
+        
+}
