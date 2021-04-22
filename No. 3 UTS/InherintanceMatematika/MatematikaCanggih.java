7dffe518d7b680a7138d6c622ea9790aa894de3e
AnnisaDwiIrvianda <80968962+AnnisaDwiIrvianda@users.noreply.github.com>
2021-04-22 09:44:10 +0700

    Add files via upload

---
+++ No. 3 UTS/InherintanceMatematika/MatematikaCanggih.java
@@ -0,0 +1,21 @@
+// Annisa Dwi Irvianda
+// 11200930000009
+
+package InherintanceMatematika;
+
+/**
+ *
+ * @author aldina safira
+ */
+public class MatematikaCanggih {
+    void pertambahan(int a, int b, int c){
+        System.out.println("jumlah pertambahan dari " + a + "+" + b + "+" + c + "="+(a+b+c));
+    }
+    void perkalian(int a, int b, int c){
+        System.out.println("jumlah perkalian dari " + a + "" + b + "" + c + "=" +(a*b*c));
+    }
+    void modulus(int a, int b, int c){
+        System.out.println("jumlah modulus dari " + a + "%" + b + "%" + c + "=" +(a%b%c));
+    
+    }
+}
---
+++ No. 3 UTS/InherintanceMatematika/MatematikaCanggihBangetBeraksi.java
@@ -0,0 +1,17 @@
+// Annisa Dwi Irvanda
+// 11200930000009
+package InherintanceMatematika;
+
+
+public class MatematikaCanggihBangetBeraksi{
+    public static void main(String[] args){
+    MatematikaCanggih jumlah = new MatematikaCanggih();
+      MatematikaCanggihBanget jumlah1 = new MatematikaCanggihBanget();{
+        
+        jumlah.pertambahan(2,3,4);
+        jumlah.perkalian(2,2,5);
+        jumlah.modulus(10,20,10);
+        jumlah1.pertambahanEmpat(1,3,6,10);
+}
+    }
+}
