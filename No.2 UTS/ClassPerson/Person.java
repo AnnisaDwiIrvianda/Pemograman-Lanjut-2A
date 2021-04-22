07c2a32a45634304c32d3741e3f30735a9adcf6a
AnnisaDwiIrvianda <80968962+AnnisaDwiIrvianda@users.noreply.github.com>
2021-04-22 09:35:13 +0700

    Add files via upload

---
+++ No.2 UTS/ClassPerson/Person.java
@@ -0,0 +1,23 @@
+// Annisa Dwi Irvianda
+// 11200930000009
+
+package ClassPerson;
+
+/**
+ *
+ * @author aldina safira
+ */
+public class Person {
+   public String Name;
+   public int Age;
+   
+   public void Person1(String Name, int Age){
+       System.out.println("My Name is:" +Name);
+       System.out.println("My Age is:" +Age);
+   }
+   public void Person2(String Name, int Age){
+       System.out.println("My Name is: "+ Name);
+       System.out.println("My Age is:" +Age);
+       
+   }
+}
---
+++ No.2 UTS/ClassPerson/PersonBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClassPerson;
+
+/**
+ *
+ * @author aldina safira
+ */
+public class PersonBeraksi {
+    public static void main(String[] args)
+    {
+        Person print = new Person ();
+        print.Person1("Joe Smith", 24);
+        print.Person2("Mary Sharp", 52);
+        
+    }
+}
